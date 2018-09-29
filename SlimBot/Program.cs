781c8d0c29d5ee1b0e335c862aea7c01238cd528
Lee <L918Sturrock@hotmail.com>
2018-09-29 23:14:11 +0100

    first commit

---
+++ SlimBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlimBot
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
