949170ccbfb2bb27baf7c25d6b35d516eab81a23
Lee <L918Sturrock@hotmail.com>
2019-06-04 09:08:38 +0100

    Implemented basic lyric grabbing command, unfortunately hindered by Discords 2000 char limit.

--- SlimBot/Commands/MiscellaneousCommands.cs
+++ SlimBot/Commands/MiscellaneousCommands.cs
@@ -1,15 +1,48 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Discord.Commands;
+using Newtonsoft.Json.Linq;
+using SlimBot.Utilities;
 
 namespace SlimBot.Commands
 {
     public class MiscellaneousCommands : ModuleBase<SocketCommandContext>
     {
-        [Command("idiot")] // Command declaration
-        [Summary("@idiot idiot :drooling_face:")] // command summary
-        public async Task Idiot() // command async task (method basically)
+        [Command("lyrics")] // Command declaration
+        [Summary("idiot")] // command summary
+        public async Task Lyrics([Remainder] string song) // command async task (method basically)
         {
-            await Context.Channel.SendMessageAsync(Context.User.Mention + " idiot"); // notify user in the text channel the command was used in
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate })) //This acts like a web browser
+            {
+                var websiteurl = $"http://lyric-api.herokuapp.com/api/find/Eminem/{song}"; // The API site
+                client.BaseAddress = new Uri(websiteurl); // Redirects our acting web browser to the API site
+                var response = client.GetAsync("").Result; // Verify connection to site
+                response.EnsureSuccessStatusCode(); // Verify connection to site
+                var result = await response.Content.ReadAsStringAsync(); // Gets full website information
+                var json = JObject.Parse(result); // Reads the json from the html
+
+                var lyrics = json["lyric"].ToString(); // Saves the detected url to lyrics string
+
+                if (lyrics == "")
+                {
+                    await ReplyAsync($"Couldn't find lyrics for `{song}`.");
+                    return;
+                }
+
+                lyrics = lyrics.RemoveExcessCharacters(1990);
+
+                lyrics = lyrics.Trim();
+
+                lyrics = lyrics.Substring(0, lyrics.LastIndexOf(" ", StringComparison.Ordinal) < 0 ? 0 : lyrics.LastIndexOf(" ", StringComparison.Ordinal));
+
+                await ReplyAsync($"Lyrics to `{song}` are...");
+                await Task.Delay(1500).ConfigureAwait(false);
+
+                await ReplyAsync($"```{lyrics}```");
+
+            }
         }
     }
 }
---
+++ SlimBot/Utilities/TextFormatting.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SlimBot.Utilities
+{
+    internal static class TextFormatting
+    {
+        public static string RemoveExcessCharacters(this string value, int maxLen)
+        {
+            return (value.Length > maxLen) ? value.Substring(0, maxLen) : value;
+        }
+    }
+}
