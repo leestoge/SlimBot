7fcb25ff3b22dad10829af2b1f469edd29c6f28e
Lee <L918Sturrock@hotmail.com>
2019-06-16 03:58:37 +0100

    Implemented basic form of $topalbum and $toptrack commands

--- SlimBot/Commands/MiscellaneousCommands.cs
+++ SlimBot/Commands/MiscellaneousCommands.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Discord.Commands;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using SlimBot.Utilities;
 
@@ -11,7 +12,7 @@ namespace SlimBot.Commands
     public class MiscellaneousCommands : ModuleBase<SocketCommandContext>
     {
         [Command("lyrics")] // Command declaration
-        [Summary("idiot")] // command summary
+        [Summary("Get lyrics from an input song")] // command summary
         public async Task Lyrics([Remainder] string song) // command async task (method basically)
         {
             using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate })) //This acts like a web browser
@@ -40,8 +41,46 @@ namespace SlimBot.Commands
                 await ReplyAsync($"Lyrics to `{song}` are...");
                 await Task.Delay(1500).ConfigureAwait(false);
 
-                await ReplyAsync($"```{lyrics}```");
+                await ReplyAsync($"```{lyrics}```");      
+            }
+        }
 
+        [Command("topalbum")] // Command declaration
+        [Summary("Check current top album")] // command summary
+        public async Task TopAlbum() // command async task (method basically)
+        {
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate })) //This acts like a web browser
+            {
+                var websiteurl = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=eminem&api_key=57ee3318536b23ee81d6b27e36997cde&format=json"; // The API site
+                client.BaseAddress = new Uri(websiteurl); // Redirects our acting web browser to the API site
+                var response = client.GetAsync("").Result; // Verify connection to site
+                response.EnsureSuccessStatusCode(); // Verify connection to site
+                var result = await response.Content.ReadAsStringAsync(); // Gets full website information
+                var dataObject = JsonConvert.DeserializeObject<dynamic>(result); // de-serialise json
+                string name = dataObject["topalbums"].album[0].name.ToString();
+                string playcount = dataObject["topalbums"].album[0].playcount.ToString();
+
+                await ReplyAsync($"Eminem's top current album is `{name}`, with a total current play count of `{playcount}`.");
+            }
+        }
+
+        [Command("toptrack")] // Command declaration
+        [Summary("Check current top track")] // command summary
+        public async Task TopTrack() // command async task (method basically)
+        {
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate })) //This acts like a web browser
+            {
+                var websiteurl = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=eminem&api_key=57ee3318536b23ee81d6b27e36997cde&format=json"; // The API site
+                client.BaseAddress = new Uri(websiteurl); // Redirects our acting web browser to the API site
+                var response = client.GetAsync("").Result; // Verify connection to site
+                response.EnsureSuccessStatusCode(); // Verify connection to site
+                var result = await response.Content.ReadAsStringAsync(); // Gets full website information
+                var dataObject = JsonConvert.DeserializeObject<dynamic>(result); // de-serialise json
+                string name = dataObject["toptracks"].track[0].name.ToString();
+                string playcount = dataObject["toptracks"].track[0].playcount.ToString();
+                string listeners = dataObject["toptracks"].track[0].listeners.ToString();
+
+                await ReplyAsync($"Eminem's top current track is `{name}`, with a total current play count of `{playcount}` and `{listeners}` total current listeners.");
             }
         }
     }
