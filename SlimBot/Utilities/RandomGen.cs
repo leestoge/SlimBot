a81a3251eae581de0d7b8f00677546a4c332a0a8
Lee <L918Sturrock@hotmail.com>
2019-06-17 16:39:49 +0100

    Created random number generator utility class

---
+++ SlimBot/Utilities/RandomGen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace SlimBot.Utilities
+{
+    internal static class RandomGen /* CODE TAKEN FROM STACKOVERFLOW, WRITTEN BY "Alessandro D'Andria" - https://stackoverflow.com/questions/19270507/correct-way-to-use-random-in-multithread-application */
+    {
+        private static int _seed;
+
+        private static readonly ThreadLocal<Random> ThreadLocal = new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref _seed)));
+
+        static RandomGen()
+        {
+            _seed = Environment.TickCount; // Seed generated from PC up-time
+        }
+
+        public static Random RandomGenInstance => ThreadLocal.Value;
+    }
+}
